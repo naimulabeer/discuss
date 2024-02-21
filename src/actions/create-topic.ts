3f08b83141f09ca26d5fb8c20a86f652176f6230
naimulabeer <naimul.h.abeer@gmail.com>
2024-02-21 17:40:53 +0600

    handling general form errors

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { z } from "zod";
+import { auth } from "@/auth";
 
 const createTopicSchema = z.object({
   name: z
@@ -16,6 +17,7 @@ interface CreateTopicFormState {
   errors: {
     name?: string[];
     description?: string[];
+    _form?: string[];
   };
 }
 
@@ -31,6 +33,15 @@ export async function createTopic(
   if (!result.success) {
     return {
       errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to do this."],
+      },
     };
   }
 
