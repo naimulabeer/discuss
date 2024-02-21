9b67162b514f83247adf7e21431de96151c54241
naimulabeer <naimul.h.abeer@gmail.com>
2024-02-21 16:49:37 +0600

    form validation with zod and using useFormState

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,10 +1,28 @@
 "use server";
 
-export async function createTopic(formData: FormData) {
-  const name = formData.get("name");
-  const description = formData.get("description");
+import { z } from "zod";
 
-  console.log(name, description);
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, {
+      message: "Must be lowercase letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
 
-  // TODO: revalidate the Homepage
+export async function createTopic(formState: number, formData: FormData) {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+  }
+
+  return 10;
+
+  // TODO: revalidate the homepage
 }
